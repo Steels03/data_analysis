4fd5ad2451d7e8835dae6c8fcd65dcd865dd4b3d
blank <blank@debian>
2022-03-28 14:15:37 +0200

    Initial commit

---
+++ td1.py
@@ -0,0 +1,33 @@
+from more_itertools import sample
+import findspark
+findspark.init()
+
+import pyspark
+from pyspark.sql import SparkSession, functions
+from pyspark.sql.functions import regexp_extract
+import re
+import pandas
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+spark = SparkSession.builder.master("local[1]").appName("SparkByExamples.com").getOrCreate()
+
+df = spark.read.text("sample_access_log")
+df.printSchema()
+print((df.count(), len(df.columns)))
+df.show(10, truncate=False)
+#sample_logs = [item['value'] for item in df.take(15)]
+#print(sample_logs)
+ts_pattern = r'\[\d{2}\/\w{3}\/\d{4}:(\d{2}):\d{2}:\d{2} \+\d{4}\]'
+timestamps_df = df.select(regexp_extract('value', ts_pattern, 1).cast("int").alias('timestamp'))
+timestamp_freq_df = (timestamps_df.groupBy('timestamp').count().sort('timestamp').cache())
+timestamp_freq_pd_df = (timestamp_freq_df.toPandas().sort_values(by=['timestamp'],ascending=True))
+#print (timestamp_freq_pd_df)
+#sns.catplot(y='count', data=timestamp_freq_pd_df, kind='bar', order=timestamp_freq_pd_df['timestamp'])
+plt.step(
+    x = timestamp_freq_pd_df['timestamp'],
+    y = timestamp_freq_pd_df['count']
+)
+plt.show()
+plt.figure.savefig('output.png')
