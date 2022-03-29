24845dae5dc2a9d95e78f5556233c9b6c7bc049f
blank <blank@debian>
2022-03-29 20:57:20 +0200

    Changed columns names, added constants.py and report.md

--- project/main.py
+++ project/main.py
@@ -1,9 +1,35 @@
+import constants
 from os.path import exists
+import findspark
+import pyspark
+from os.path import exists
+import numpy as np
+from pyspark.ml.feature import CountVectorizer
+from pyspark.ml.feature import Tokenizer, RegexTokenizer
+from pyspark.ml.feature import StringIndexer
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.classification import NaiveBayes
+from functools import reduce
 
-data_file = "project/dataset/index.html"
+findspark.init()
 
-if (not exists(data_file)):
-    print("ERROR : " + data_file + " isn't present")
-    exit(0)
+dataset_file = "project/dataset/KDDTrain+.txt"
+
+spark = pyspark.sql.SparkSession.builder.appName("project").getOrCreate()
+
+#check if dataset is present
+if (not exists(dataset_file)):
+    print("ERROR : " + dataset_file + " isn't present")
+    exit(1)
 else:
     print("dataset is present")
+
+df = spark.read.format("csv").load(dataset_file)
+df.limit(10).show()
+df.printSchema()
+
+oldColumns = df.schema.names 
+
+df = reduce(lambda df, idx: df.withColumnRenamed(oldColumns[idx], constants.COLUMN_NAMES[idx]), range(len(oldColumns)), df)
+df.printSchema()
+df.limit(10).show()
